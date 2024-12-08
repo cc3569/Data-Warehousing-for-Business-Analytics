5599540117763af58b5852b195888a1c42bbe797
Cole T. Catron, B.S., B.S.B.A. <cc3569@nau.edu>
2024-12-07 18:47:21 -0700

    Delete SQL_script_arizona_sales_in_2018.sql

--- SQL_script_arizona_sales_in_2018.sql
+++
@@ -1,3 +0,0 @@
-SELECT Customers.CustomerName, States.State, Dates.Date, Orders.Sales
-FROM States INNER JOIN (Customers INNER JOIN (Dates INNER JOIN Orders ON Dates.DateKey = Orders.DateKey) ON Customers.CustomerKey = Orders.CustomerKey) ON States.StateKey = Customers.StateKey
-WHERE (((States.State)="Arizona") AND ((Dates.Date) Between #1/1/2018# And #12/31/2018#));
